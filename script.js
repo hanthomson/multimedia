7fa4e2fa508bc452489f089a3987da552d054adc
hanthomson <49211012+hanthomson@users.noreply.github.com>
2019-04-02 21:09:43 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    var currentDate = new Date(),
+         day = currentDate.getDate(),
+         month = currentDate.getMonth() + 1,
+         year = currentDate.getFullYear();
+    document.getElementById('txt2').innerHTML = h + ":" + m + ":" + s + "   " + day + "/" + month + "/" + year;
+    var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};
+    return i;
+}
+
+
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 3000); // Change image every 2 seconds
+}
+
+const menuToggle = document.getElementById("menu-toggle");
+const menuNav = document.getElementById("menu-nav");
+
+const toggleMenu = () => {
+  console.log("called toggleMenu")
+  menuNav.classList.toggle("menu-toggle");
+}
